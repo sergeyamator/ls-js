8646a1486b5a36f9526b211a07cc4048850f75c2
Sergey Rubets <sramator@gmail.com>
2016-08-01 11:29:23 +0300

    Addcookie function was added

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
 require('./modules/renderCookies');
+let addCookie = require('./modules/addCookie');
+
+document.forms[0].addEventListener('submit', (e) => {
+  e.preventDefault();
+  addCookie(e.target);
+});
---
+++ dev/js/modules/addCookie.js
@@ -0,0 +1,34 @@
+'use strict';
+
+function addCookie(form) {
+  let name = form.querySelector('[data-cookie="name"]').value,
+    value = form.querySelector('[data-cookie="value"]').value,
+    day = Number(form.querySelector('[data-cookie="day"]').value),
+    toDay = new Date();
+
+  if (isValid(form)) {
+    toDay.setDate(toDay.getDate() + day);
+    document.cookie = `${name} = ${value}; expires = ${toDay.toUTCString()}`;
+  } else {
+    throw new Error('Заполнены не все поля');
+  }
+}
+
+function isValid(form) {
+  if (!form) return;
+  let inputs = form.querySelectorAll('input');
+  let isValid = null;
+
+  for (let i = 0; i < inputs.length; i++) {
+    if (!inputs[i].value) {
+      isValid = false;
+      break;
+    } else {
+      isValid = true;
+    }
+  }
+
+  return isValid;
+}
+
+module.exports = addCookie;
